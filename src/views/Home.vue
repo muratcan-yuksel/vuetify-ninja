8da30381a142ac67069e05c413fddfc00d33b02b
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-06 10:39:24 +0300

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
-    <v-btn depressed elevation="2" icon></v-btn>
-    hey
+    <h1>homepage</h1>
+    <p class="red white--text">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+      voluptatum vel delectus harum praesentium vero enim illo dolore
+      perspiciatis eius explicabo beatae consequatur iusto vitae, magnam quas
+      consequuntur aspernatur amet.
+    </p>
+    <p class="pink lighten-4 red--text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste officiis est
+      sequi eveniet molestiae blanditiis suscipit. Consequuntur eum at dolores
+      tenetur dolorem in! In voluptas obcaecati error vero aperiam illum.
+    </p>
+    <h1 class="display-4">massive display</h1>
+    <h1 class="display-1">smaller display</h1>
   </div>
 </template>
 

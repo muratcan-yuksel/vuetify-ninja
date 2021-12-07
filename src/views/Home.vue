6ae5a6b76f285f78d7f8180f1f60c1c62a210d73
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-07 10:24:08 +0300

    starting project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,8 @@
 <template>
   <div>
-    <h1>homepage</h1>
-    <p class="red white--text">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
-      voluptatum vel delectus harum praesentium vero enim illo dolore
-      perspiciatis eius explicabo beatae consequatur iusto vitae, magnam quas
-      consequuntur aspernatur amet.
-    </p>
-    <p class="pink lighten-4 red--text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste officiis est
-      sequi eveniet molestiae blanditiis suscipit. Consequuntur eum at dolores
-      tenetur dolorem in! In voluptas obcaecati error vero aperiam illum.
-    </p>
-    <h1 class="display-4">massive display</h1>
-    <h1 class="display-1">smaller display</h1>
+    <h1>Homepage</h1>
+    <v-btn class="hidden-md-and-up">Click me</v-btn>
+    <v-btn class="hidden-sm-only">Hidden only on small screens</v-btn>
   </div>
 </template>
 
